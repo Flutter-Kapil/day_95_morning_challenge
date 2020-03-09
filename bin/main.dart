f6b172e29260843bb28b592cbd504f9320fddb5e
kapil jhajhria <jhajhria44@gmail.com>
2020-03-09 09:28:29 +0530

    bug fix for challenge 1

--- bin/main.dart
+++ bin/main.dart
@@ -25,7 +25,6 @@
 //   }
 // ]) ➞ 3
 
-
 // Challenge 2
 // This problem was asked by Microsoft.
 //
@@ -40,47 +39,40 @@
 // Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the
 // string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 // ['bedbath', 'and', 'beyond'].
-int longestStreak(List<Map> days){
+int longestStreak(List<Map> days) {
   days.sort((a, b) => a['date'].compareTo(b['date']));
   print(days);
-  int longestStreak=1;
-  int temp=1;
-  for(int i=0;i<days.length-1;i++){
-    if(differenceBetweenDays(days[i]['date'], days[i+1]['date'])==1){
+  int longestStreak = 1;
+  int temp = 1;
+  for (int i = 0; i < days.length - 1; i++) {
+    if (differenceBetweenDays(days[i]['date'], days[i + 1]['date']) == 1) {
       temp++;
-    }else{
-      if(longestStreak<temp){
-        longestStreak=temp;
-      }
-       temp=0;
+    } else {
+      temp = 0;
+    }
+    if (longestStreak < temp) {
+      longestStreak = temp;
     }
   }
   return longestStreak;
 }
 
-int differenceBetweenDays(String day1, String day2){
+int differenceBetweenDays(String day1, String day2) {
   return DateTime.parse(day2).difference(DateTime.parse(day1)).inDays;
 }
+
 void main() {
-  print( longestStreak([
-    {
-      "date": "2019-09-18"
-    },
-    {
-      "date": "2019-09-19"
-    },
-    {
-      "date": "2019-09-26"
-    },
-    {
-      "date": "2019-09-27"
-    },
-    {
-      "date": "2019-09-30"
-    },
-    {
-      "date": "2019-09-20"
-    },
+  print(longestStreak([
+    {"date": "2019-09-18"},
+    {"date": "2019-09-19"},
+    {"date": "2019-09-26"},
+    {"date": "2019-09-27"},
+    {"date": "2019-09-30"},
+    {"date": "2019-09-20"},
+  ]));
+  print(longestStreak([
+    {"date": "2019-09-18"},
+    {"date": "2019-09-19"},
   ]));
 
   // For example, given the set of words 'quick', 'brown', 'the', 'fox', and the
@@ -89,35 +81,35 @@ void main() {
 // Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the
 // string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 // ['bedbath', 'and', 'beyond'].
-print(originalString(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
-  print(originalString(['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+  print(originalString(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
+  print(originalString(
+      ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
 }
 
-List<String> originalString(List<String> words, String noSpaceString){
-  List<String> res=[];
+List<String> originalString(List<String> words, String noSpaceString) {
+  List<String> res = [];
 
-  if(words.length==0||noSpaceString.length==0){
+  if (words.length == 0 || noSpaceString.length == 0) {
     return [];
   }
 
-  for(int i=0;i<words.length;i++){
-    String currentWord=words[i];
-    int lengthToTrim= currentWord.length;
-    if(matchWord(currentWord, noSpaceString)){
+  for (int i = 0; i < words.length; i++) {
+    String currentWord = words[i];
+    int lengthToTrim = currentWord.length;
+    if (matchWord(currentWord, noSpaceString)) {
       res.add(currentWord);
-      noSpaceString=noSpaceString.substring(lengthToTrim);
+      noSpaceString = noSpaceString.substring(lengthToTrim);
       words.removeAt(i);
       res.addAll(originalString(words, noSpaceString));
     }
   }
 
-
-  return res.isEmpty?null:res;
+  return res.isEmpty ? null : res;
 }
 
-bool matchWord(String currentWord, String noSpaceString){
-  for(int i=0;i<currentWord.length;i++){
-    if(currentWord[i]!=noSpaceString[i]){
+bool matchWord(String currentWord, String noSpaceString) {
+  for (int i = 0; i < currentWord.length; i++) {
+    if (currentWord[i] != noSpaceString[i]) {
       return false;
     }
   }

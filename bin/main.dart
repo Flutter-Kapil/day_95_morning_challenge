816a7b7e588a38cae5f9dfd61b91515c5a4d9bac
kapil jhajhria <jhajhria44@gmail.com>
2020-03-13 12:10:31 +0530

    only one edge case is failing day 98

--- bin/main.dart
+++ bin/main.dart
@@ -39,6 +39,8 @@
 // Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the
 // string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 // ['bedbath', 'and', 'beyond'].
+import 'dart:math';
+
 int longestStreak(List<Map> days) {
   if (days.isEmpty) return 0;
   days.sort((a, b) => a['date'].compareTo(b['date']));
@@ -86,24 +88,63 @@ void main() {
         {"date": "2019-10-19"},
       ]) ==
       1);
-
+  print("test 1");
   print(originalString(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
+  print(originalString2(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
+  print("------------");
+  print("test 2");
   print(originalString(
       ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+  print(originalString2(
+      ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+  print("------------");
+  print("test 3");
   print(originalString(['bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
+  print(originalString2(['bed', 'bath', 'bedbath', 'and', 'beyond'],
+      "bedbathandbeyondeverything"));
+  print("------------");
+  print("test 4");
   print(originalString(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
+  print(originalString2(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
+      "bedbathandbeyondeverything"));
+  print("------------");
+  print("test 5");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'],
       'thatfoxisthequickbrownfox'));
+  print(originalString2(
+      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'],
+      'thatfoxisthequickbrownfox'));
+  print("------------");
+  print("test 6");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], 'fox'));
+  print(originalString2(
+      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], 'fox'));
+  print("------------");
+  print("test 7");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], ''));
+  print(originalString2(
+      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], ''));
+  print("------------");
+  print("test 8");
   print(originalString(
+      ['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+//  print(randomString(100000));
+//  print(originalString(['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
+//      multiplString(10000, "bedbathandbeyond")));
+  print(originalString2(
+      ['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+  print("------------");
+  print("test 9");
+  print(originalString2(
       ['bedb','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-
+  print(originalString2(
+      ['be','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
+  print("------------");
 }
 
 List<String> originalString(List<String> dictionary, String noSpaceString) {
@@ -132,4 +173,73 @@ List<String> originalString(List<String> dictionary, String noSpaceString) {
 
     return (temp != null) ? res : temp;
   }
+}
+
+String randomString(length) {
+  var result = '';
+  var characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters[Random().nextInt(26)]; //Random().nextInt(26)
+  }
+  return result;
+}
+
+multiplString(int n, String str) {
+  String res = '';
+  for (int i = 0; i <= n; i++) {
+    res = res + str;
+  }
+  return res;
+}
+
+List<String> originalString2(List<String> dictionary, String noSpaceString) {
+  List<String> res = [];
+
+  if (noSpaceString.length == 0) {
+    return null;
+  }
+    String prevValue='';
+  mainLoop:
+  while (noSpaceString.length != 0) {
+    for (int i = 0; i < noSpaceString.length; i++) {
+      String currentWord = noSpaceString.substring(0, i + 1);
+      int lengthToTrim = currentWord.length;
+      if (dictionary.contains(currentWord)) {
+        res.add(currentWord);
+        prevValue = noSpaceString;
+        noSpaceString = noSpaceString.substring(lengthToTrim);
+        continue mainLoop;
+      }
+//      else if(i==noSpaceString.length-1){
+//        //this means there is something remaining and its not present in dictionary as well, so we return null
+//        break;
+//      }
+    }
+    break mainLoop;
+  }
+    if (res.isEmpty) {
+//      print(1);
+      return null;
+    } else if (noSpaceString.isEmpty) {
+      print(2);
+      return res;
+    }
+    else if(noSpaceString.isNotEmpty && res.isNotEmpty){
+//      print("nospaceString is $noSpaceString and res is $res ");
+      print(3);
+      return null;
+    }
+    print(4);
+
+//  else {
+//    List<String> temp = originalString(dictionary, noSpaceString);
+//    if (temp != null) res.addAll(temp);
+//
+//    return (temp != null) ? res : temp;
+//  }
+
+
+  return res;
 }

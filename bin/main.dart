66c0d18115d4362831f8f46e661529eda86f9da1
kapiljhajhria <Aerospace44>
2020-03-17 10:31:32 +0530

    code cleanup

--- bin/main.dart
+++ bin/main.dart
@@ -90,91 +90,66 @@ void main() {
       1);
   print("test 1");
   print(originalString(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
-  print(originalString2(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
-  print(originalString3(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
+  print(originalStringWithoutRecur(['quick', 'brown', 'the', 'fox'], "thequickbrownfox"));
   print("------------");
   print("test 2");
   print(originalString(
       ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString2(
-      ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
   print("------------");
   print("test 3");
   print(originalString(['bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
-  print(originalString2(['bed', 'bath', 'bedbath', 'and', 'beyond'],
-      "bedbathandbeyondeverything"));
-  print(originalString3(['bed', 'bath', 'bedbath', 'and', 'beyond'],
+  print(originalStringWithoutRecur(['bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
   print("------------");
   print("test 4");
   print(originalString(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
-  print(originalString2(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
-      "bedbathandbeyondeverything"));
-  print(originalString3(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
+  print(originalStringWithoutRecur(['bedbath', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
       "bedbathandbeyondeverything"));
   print("------------");
   print("test 5");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'],
       'thatfoxisthequickbrownfox'));
-  print(originalString2(
-      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'],
-      'thatfoxisthequickbrownfox'));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'],
       'thatfoxisthequickbrownfox'));
   print("------------");
   print("test 6");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], 'fox'));
-  print(originalString2(
-      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], 'fox'));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], 'fox'));
   print("------------");
   print("test 7");
   print(originalString(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], ''));
-  print(originalString2(
-      ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], ''));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['quick', 'brown', 'the', 'fox', 'quickbrown', 'that', 'is'], ''));
   print("------------");
   print("test 8");
   print(originalString(
       ['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-//  print(randomString(100000));
-//  print(originalString(['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'],
-//      multiplString(10000, "bedbathandbeyond")));
-  print(originalString2(
-      ['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['bedb', 'bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
   print("------------");
   print("test 9");
   print(originalString(
       ['bedb','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString2(
-      ['bedb','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['bedb','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
   print("test 10");
   print(originalString(
       ['be','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString2(
+  print(originalStringWithoutRecur(
       ['be','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString3(
-      ['be','bed', 'bath', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print("------------111");
+  print("test 11");
   print(originalString(
       ['bed', 'bat', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString2(
-      ['bed', 'bat', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
-  print(originalString3(
+  print(originalStringWithoutRecur(
       ['bed', 'bat', 'bedbath', 'and', 'beyond'], "bedbathandbeyond"));
 }
 
@@ -217,71 +192,8 @@ String randomString(length) {
   return result;
 }
 
-multiplString(int n, String str) {
-  String res = '';
-  for (int i = 0; i <= n; i++) {
-    res = res + str;
-  }
-  return res;
-}
 
-List<String> originalString2(List<String> dictionary, String noSpaceString) {
-
-  int loops=0;
-  List<String> res = [];
-  List<String> possibilities=[];
-  if (noSpaceString.length == 0) {
-    return null;
-  }
-  mainLoop:
-  while (noSpaceString.length != 0) {
-    for (int i = 0; i < noSpaceString.length; i++) {
-      String currentWord = noSpaceString.substring(0, i + 1);
-      int lengthToTrim = currentWord.length;
-      if (dictionary.contains(currentWord)) {
-        res.add(currentWord);
-        possibilities.add(currentWord);
-        noSpaceString = noSpaceString.substring(lengthToTrim);
-        ///TODO
-        continue mainLoop;
-      }
-//      else if(i==noSpaceString.length-1){
-//        //this means there is something remaining and its not present in dictionary as well, so we return null
-//        break;
-//      }
-    }
-    break mainLoop;
-  }
-    if (res.isEmpty) {
-//      print(1);
-      return null;
-    } else if (noSpaceString.isEmpty) {
-      print(2);
-      return res;
-    }
-    else if(noSpaceString.isNotEmpty && res.isNotEmpty){
-//      print("nospaceString is $noSpaceString and res is $res ");
-      print(3);
-      //can add another while llop here, which goes through each starting position, and then
-      possibilities=possibilities.toSet().toList();
-      if(loops<=possibilities.length){
-        String temp =possibilities[0];
-        dictionary.remove(temp);
-        dictionary.add(temp);
-        possibilities.removeAt(0);
-        possibilities.add(temp);
-        print("inside loops");
-        loops++;
-      }
-
-      return null;
-    }
-    print(4);
-
-  return res;
-}
-
-List<String> originalString3(List<String> dict, String str) {
+List<String> originalStringWithoutRecur(List<String> dict, String str) {
 //  print("is str empty ${str.isEmpty} length ${str.length}");
   if(str.isEmpty)
     return null;
